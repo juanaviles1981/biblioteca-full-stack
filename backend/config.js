1e776056f0daebe57fadaf64ef1f6f7179ca550f
juanaviles1981 <juanaviles1981@gmail.com>
2025-11-04 13:24:21 -0300

    guardar y listar libros

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const PORT = 3000;
+export const PORT = 5555;
 export const mongoDBURL = 'mongodb+srv://juanaviles1981_db_user:Dbck1981!!!!@biblioteca.xio5gjd.mongodb.net/?appName=biblioteca'
