da67158ac9683b3f96f31550e1269c8fe69bddf9
Juan Aviles <juanaviles1981@gmail.com>
2025-11-09 05:24:06 -0300

    form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    nombre: "",
+    autor: ""
+  });
+  
+  function handleChange(e) {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
 
+  function handleSubmit() {
+    console.log("Enviando el formulario con el libro:", formData);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>Biblioteca</h1>
+        <form action="">
+        <input
+          type="text"
+          name="nombre"
+          value={formData.nombre}
+          onChange={handleChange}
+          placeholder="Ingrese nombre libro"
+        />
+        <input type="text"
+        name="autor"
+        value={formData.autor}
+        onChange={handleChange}
+        placeholder="Ingrese autor libro" />
+
+        <p>{formData.nombre}</p>
+        <p>{formData.autor}</p>
+        <button type="button"
+        onClick={handleSubmit}>Guardar</button>
+      </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
